ea8d836876141f54b3a474db43e6abf6c61915a8
kellywangq <76407900+kellywangq@users.noreply.github.com>
2026-01-21 09:38:17 +0000

    Deploying to gh-pages from @ kellywangq/kellywangq.github.io@25be39fbe88d1a2cf750eb3bd58b17e20f5230a9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -49,12 +49,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "teachings-introduction-to-machine-learning",
-          title: 'Introduction to Machine Learning',
-          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/introduction-to-machine-learning/";
-            },},{
+          section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
